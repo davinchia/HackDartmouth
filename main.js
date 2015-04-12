490f9319c064a356178d327a33113a531942f33a
davinchia <davinchia@outlook.com>
2015-04-12 02:27:57 -0400

    drawing lines between cities

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function calculatePoint(lat, lon) {
  
 
 	answer = [x, y, z];
-	console.log(answer);
+	//console.log(answer);
 	return answer;
 }
 
@@ -24,7 +24,6 @@ function calculatePointM(lat, lon, offset) {
  
 
 	answer = [x, y, z];
-	console.log(answer);
 	return answer;
 }
 
@@ -43,20 +42,66 @@ function calculateSpline(slat, slon, elat, elon, offset) {
  
 
 	answer = [x, y, z];
-	//console.log(answer);
 	return answer;
 }
 
-function Sphere() {
-	this.children = [];
 
-	this.addChild = function(child) {
-		this.children.add(child);
-	}
-	return 
+//reference points for countries
+function initCountry() {
+	//reference points for countries
+	var countriesLL = [
+		["Canada", 65, -95 ],
+		["Mexico", 19, -102.4],
+		["China", 35, 103],
+		["Japan", 35, 136],
+		["UK", 54, -2],
+		["Germany", 51.6, 10],
+		["Brazil", -10.7, -53],
+		["Netherlands", 52.2, 4.5],
+		["HK", 22.3, 114.2],
+		["Korea", 37.6, 127],
+		["Saudi", 24, 45],
+		["France", 47, 2],
+		["India", 21, 78],
+		["US", 38.9, -90.01]
+	
+	];
+	
+	var countries = {};
+	
+	for (var i = 0; i < countriesLL.length; i++) {
+		var key = countriesLL[i][0];
+		var lat = countriesLL[i][1];
+		var lon = countriesLL[i][2];
+		//console.log(key);
+		//calculatePoint(countriesLL[i][0], countries.countriesLL[i][1]);
+		var data = calculatePoint(lat, lon);
+		countries[key] = data;
+	};
+	
+
+	//console.log(1);
+	//console.log(countries["Canada"]);
+	//console.log(calculatePoint(65, -95))
+	//console.log(Object.keys(countries));
+	return countries;
 }
 
-function addCurve(arr1, arr2, arr3, object) {
+function calculateSpline1(origin, dest) {
+    var p1 = new THREE.Vector3().fromArray(origin);
+    var p2 = new THREE.Vector3().fromArray(dest);
+    console.log(p1);
+    console.log(p2);
+    var p3 = new THREE.Vector3()
+    p3.addVectors(p1, p2)
+    p3.normalize()
+    p3.multiplyScalar(2)
+    console.log("p3");
+    console.log(p3)
+    return p3.toArray();
+}
+
+function addCurve(arr1, arr2, arr3, object, geometry1, material1) {
 	var curve = new THREE.QuadraticBezierCurve3(
 		new THREE.Vector3( arr1[0], arr1[1], arr1[2]),
 		new THREE.Vector3( arr2[0], arr2[1], arr2[2]),
@@ -67,7 +112,7 @@ function addCurve(arr1, arr2, arr3, object) {
 	geometry.vertices = curve.getPoints( 500 );
 
 	var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-	material.linewidth = 3;
+	material.linewidth = 2;
 	material.opacity = 1;
 	material.transparent = true;
 	console.log(material);
@@ -75,6 +120,12 @@ function addCurve(arr1, arr2, arr3, object) {
 	var curveObject = new THREE.Line( geometry, material );
 
 	object.add(curveObject);
+
+	var city = new THREE.Mesh( geometry1, material1 );
+	city.translateX(arr3[0]);
+	city.translateY(arr3[1]);
+	city.translateZ(arr3[2]);
+	object.add(city);
 }
 
 window.onload = function() {
@@ -102,67 +153,93 @@ window.onload = function() {
 	
 	var sphere1 = new THREE.Mesh( geometry, material );
 	var material1 = new THREE.MeshPhongMaterial({color: 0xFF0000});
-	var geometry1 = new THREE.SphereGeometry( 0.01, 32, 32 )
+	var geometry1 = new THREE.SphereGeometry( 0.02, 32, 32 )
 	var sphere2 = new THREE.Mesh( geometry1, material1 );
 	
 	scene.add( sphere1 );
 	//sphere.translateX()
 	
 	//0, 0
-	var translate1 = calculatePoint(0, 0);
-	sphere2.translateX(translate1[0]);
-	sphere2.translateY(translate1[1]);
-	sphere2.translateZ(translate1[2]);
+	// var translate1 = calculatePoint(0, 0);
+	// sphere2.translateX(translate1[0]);
+	// sphere2.translateY(translate1[1]);
+	// sphere2.translateZ(translate1[2]);
 
-	sphere1.add( sphere2 );
+	//sphere1.add( sphere2 );
 
 	//180, 180
-	var sphere3 = new THREE.Mesh( geometry1, material1 );
-	var translate2 = calculatePoint(20, 0);
-	sphere3.translateX(translate2[0]);
-	sphere3.translateY(translate2[1]);
-	sphere3.translateZ(translate2[2]);
-	sphere1.add( sphere3 );
+	// var sphere3 = new THREE.Mesh( geometry1, material1 );
+	// var translate2 = calculatePoint(20, 0);
+	// sphere3.translateX(translate2[0]);
+	// sphere3.translateY(translate2[1]);
+	// sphere3.translateZ(translate2[2]);
+	// sphere1.add( sphere3 );
 	
 	//90, 0
-	var sphere4 = new THREE.Mesh( geometry1, material1 );
-	var translate3 = calculatePoint(-22.9, -43.1);
-	sphere4.translateX(translate3[0]);
-	sphere4.translateY(translate3[1]);
-	sphere4.translateZ(translate3[2]);
-	sphere1.add( sphere4 );
+	// var sphere4 = new THREE.Mesh( geometry1, material1 );
+	// var translate3 = calculatePoint(-22.9, -43.1);
+	// sphere4.translateX(translate3[0]);
+	// sphere4.translateY(translate3[1]);
+	// sphere4.translateZ(translate3[2]);
+	// sphere1.add( sphere4 );
 
-	var sphere5 = new THREE.Mesh( geometry1, material1 );
-	var translate4 = calculatePoint(40.71, -74.01);
-	sphere5.translateX(translate4[0]);
-	sphere5.translateY(translate4[1]);
-	sphere5.translateZ(translate4[2]);
-	sphere1.add( sphere5 );
+	// var sphere5 = new THREE.Mesh( geometry1, material1 );
+	// var translate4 = calculatePoint(40.71, -74.01);
+	// sphere5.translateX(translate4[0]);
+	// sphere5.translateY(translate4[1]);
+	// sphere5.translateZ(translate4[2]);
+	// sphere1.add( sphere5 );
 	
-	var mid = calculateSpline(-22.9, -43.1, 40.71, -74.01, 1.5);
-	//var mid1 = calculatePointM(-22.9, -43.1, 1.5);
-	//var mid2 = calculatePointM(40.71, -74.01, 1.5);
+	//var mid = calculateSpline(-22.9, -43.1, 40.71, -74.01, 1.5);
 
-	addCurve(translate3, mid, translate4, sphere1);
+	//reference countries
+	var countries = initCountry();
+	
+	//US export then import
+	var USEcondata = [
+		[
+			["Canada", 300.2], ["Mexico", 226.2], ["China", 122.1], ["Japan", 65.2],
+			["UK", 47.4], ["Germany",47], ["Brazil",44.2], ["Netherlands", 42.7],
+			["HK", 42.3], ["Korea", 41.5]
+		],
+		[
+			["China", 460], ["Canada", 336.6], ["Mexico", 283.1], ["Japan", 143.1],
+			["Germany", 116.9], ["Korea",65], ["UK",53.6], ["Saudi", 53.1],
+			["France", 46.4], ["India", 43.3]
+		]
+	]
 
-	// var curve = new THREE.QuadraticBezierCurve3(
-	// 	new THREE.Vector3( translate3[0], translate3[1], translate3[2]),
-	// 	new THREE.Vector3( mid[0], mid[1], mid[2]),
-	// 	new THREE.Vector3( translate4[0], translate4[1], translate4[2])
-	// );
+	//new points
+	//addCurve(translate3, mid, translate4, sphere1);
 
-	// var geometry = new THREE.Geometry();
-	// geometry.vertices = curve.getPoints( 500 );
+	//add data points to world
+	var origin = countries["US"];
+	var us = new THREE.Mesh( geometry1, material1 );
+	us.translateX(origin[0]);
+	us.translateY(origin[1]);
+	us.translateZ(origin[2]);
+	sphere1.add(us);
+	//console.log(origin);
+	//origin = new THREE.Vector3().fromArray(origin)
+	//exports
+	var geometry2 = new THREE.SphereGeometry( 0.015, 32, 32 )
+	for (var i = 0; i < USEcondata[0].length; i++) {
+		var key = USEcondata[0][i][0];
+		//var lat = referenceAr[key
+		console.log(key);
+		console.log(countries[key]);
+		var dest = countries[key];
+		//add the line
+		var mid = calculateSpline1(origin, dest);
+		console.log("mid");
+		console.log(mid);
+		addCurve(origin, mid, dest, sphere1, geometry2, material1);
 
-	// var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-	// material.linewidth = 3;
-	// material.opacity = 1;
-	// material.transparent = true;
-	// console.log(material);
-	// // Create the final Object3d to add to the scene
-	// var curveObject = new THREE.Line( geometry, material );
+		//add destination
 
-	// sphere1.add(curveObject);
+	}
+
+
 
 	var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
 	scene.add( camera )
